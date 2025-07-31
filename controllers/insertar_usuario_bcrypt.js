0160a77f606c4f54afe55da9161003a32da0eabf
pedromelendezrivera <pedromelendezrivera@gmail.com>
2025-07-31 17:21:09 -0500

    se agrego script para agregar usuario a la BD insertar_usuario_bcrypt.js

---
+++ controllers/insertar_usuario_bcrypt.js
@@ -0,0 +1,39 @@
+
+const mysql = require('mysql2');
+const bcrypt = require('bcrypt');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'admin',
+  database: 'tienda'
+});
+
+connection.connect(async (err) => {
+  if (err) {
+    return console.error('Error de conexión:', err.message);
+  }
+  console.log('Conectado a la base de datos');
+
+  const username = 'user';
+  const plainPassword = 'user123';
+
+  try {
+    const hashedPassword = await bcrypt.hash(plainPassword, 10);
+
+    const sql = 'INSERT INTO usuarios (username, password) VALUES (?, ?) ON DUPLICATE KEY UPDATE password = ?';
+    const values = [username, hashedPassword, hashedPassword];
+
+    connection.query(sql, values, (error, results) => {
+      if (error) {
+        console.error('Error al insertar:', error.message);
+      } else {
+        console.log('Usuario insertado o actualizado correctamente.');
+      }
+      connection.end();
+    });
+  } catch (err) {
+    console.error('Error al encriptar contraseña:', err.message);
+    connection.end();
+  }
+});
--- controllers/menu.js
+++ controllers/menu.js
@@ -1,11 +1,17 @@
+window.addEventListener('DOMContentLoaded', () => {
+    // Redirige si no hay token
+    if (!localStorage.getItem('token')) {
+        window.location.href = 'login.html';
+    }
+
+    // Prevenir navegación hacia atrás
+    history.pushState(null, document.title, location.href);
+    window.addEventListener('popstate', function (event) {
+        history.pushState(null, document.title, location.href);
+    });
+});
+
 function logout() {
     localStorage.clear();
     window.location.href = 'login.html';
 }
-
-
-window.addEventListener('DOMContentLoaded', () => {
-    if (!localStorage.getItem('token')) {
-        window.location.href = 'login.html';
-    }
-});
